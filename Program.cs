4da73da28af41dffaed948806ef3a758fb75e23c
MNS81 <0x4c0656dc@gmail.com>
2024-03-14 15:46:39 +0300

    Оптимизация кода

--- Program.cs
+++ Program.cs
@@ -1,43 +1,27 @@
 ﻿using System.Globalization;
 
-var date = new VasyaPage();
-date.ShowRealDate();
-
 class VasyaPage
 {
     public int day;
     public string month;
 
-    public VasyaPage()
-    {
-        string input = Console.ReadLine();
-        if (!String.IsNullOrEmpty(input))
-        {
-            var date = input.Split();
-            day = int.Parse(date[0]);
-            month = date[1];
-        }
-    }
-
-    public void ShowRealDate()
+    public int GetDaysCount()
     {
         int count = 0;
-        bool isEqual = false;
         var months = new string[] { "августа", "апреля", "декабря", "июля", "июня", "мая", "марта", "ноября", "октября", "сентября", "декабря" };
         foreach (string mm in months)
-        {
-            if (isEqual == true) { break; }
             for (int dd = 2; dd < 31; dd += 2)
             {
                 count++;
                 if (dd == day && mm == month)
-                {
-                    isEqual = true;
-                    break;
-                }  
+                    return count;
             }
-        }
-        var nextDateTime = new DateTime().AddDays(count);
+        return count;
+    }
+
+    public void ShowRealDate()
+    {
+        var nextDateTime = new DateTime().AddDays(GetDaysCount());
         Console.WriteLine(nextDateTime.ToString("M", CultureInfo.GetCultureInfo("ru-RU")));
     }
 }
